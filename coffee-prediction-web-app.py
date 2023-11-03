332753ed61fe465c3cc80c495af3b64c673a6d95
darahmanayaga <darahaeyo.darah@gmail.com>
2023-11-04 02:12:04 +0800

    testing

--- coffee-prediction-web-app.py
+++ coffee-prediction-web-app.py
@@ -23,3 +23,37 @@ def main():
     st.title('Coffee Yield Prediction Web App')
 
     # Getting input data from the user
+    weathercode = st.text_input('Weathercode')
+    temperature_2m = st.text_input('Temperature in °C')
+    relativehumidity_2m = st.text_input('Relative Humidity')
+    dewpoint_2m = st.text_input('Dewpoint in °C')
+    rain = st.text_input('Rain')
+    surface_pressure = st.text_input('Surface Pressure')
+    cloudcover = st.text_input('Cloudcover')
+    et0_fao_evapotranspiration = st.text_input('Evapotranspiration')
+    vapor_pressure_deficit = st.text_input("Vapor Pressure Deficit")
+    Wind_Direction = st.text_input("Wind Direction")
+    Wind_Speed = st.text_input("Wind Speed")
+    windgusts_10m = st.text_input("Windgusts")
+    soil_temperature_28_to_100cm = st.text_input("Soil Temperature in °C")
+    soil_moisture_28_to_100cm = st.text_input("Soil Moisture")
+    direct_radiation = st.text_input("Direct Radiation")
+    diffuse_radiation = st.text_input("Diffuse Radiation")
+
+    # Code for prediction
+    Yield = ''
+
+    # Button for prediction
+    if st.button('Predicted Coffee Yield'):
+        Yield = coffee_prediction([weathercode,temperature_2m,relativehumidity_2m,dewpoint_2m,
+                                    rain,surface_pressure,cloudcover,et0_fao_evapotranspiration,
+                                    vapor_pressure_deficit,Wind_Direction,Wind_Speed,windgusts_10m,
+                                    soil_temperature_28_to_100cm,soil_moisture_28_to_100cm,
+                                    direct_radiation,diffuse_radiation])
+    
+    st.success(Yield)
+
+
+
+if __name__ == '__main__':
+    main()
